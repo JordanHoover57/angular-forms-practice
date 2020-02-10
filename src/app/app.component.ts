00bd1018a9310c661d2d1c746dc83023118ecddc
Jordan <synesthesia57@gmail.com>
2020-02-09 19:51:48 -0500

    initial commit JH 02092020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ['Angular','React','Vue'];
+
+  userModel = new User('', 'Jordan@gmail.com',8142442888,'','morning',true);
 }
